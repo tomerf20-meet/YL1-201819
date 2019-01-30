e4ea163c12a92f317d61684a22b97a084018084a
tomerf20-meet <tomerf20@meet.mit.edu>
2019-01-30 10:47:37 -0500

    agario

--- agario.py
+++ agario.py
@@ -3,24 +3,26 @@ import time
 import random
 import math
 from ball import Ball
+turtle.register_shape("pic.gif")
+turtle.bgpic("pic.gif") #making a background
 turtle.colormode(1)
 turtle.tracer(0)
 turtle.ht()
 running = True
 screen_width = turtle.getcanvas().winfo_width()/2
 screen_height = turtle.getcanvas().winfo_height()/2
-my_ball = Ball(100, 400, 5, 5, 30, "red")
-your_ball = Ball(100, 400, 5, 5, 30, "blue")
-number_of_balls = 5
-minimum_ball_radius = 10
-maximum_ball_radius = 100
-maximum_ball_dx = 5
+my_ball = Ball(100, 100, 5, 5, 30, "red") #creating the balls that the players will use
+your_ball = Ball(100, 100, 5, 5, 30, "blue")
+number_of_balls = 5 #making sure that there will always be a certain number that will move automatically
+minimum_ball_radius = 10 #giving a range of values to the radiuses of the random help
+maximum_ball_radius = 100 
+maximum_ball_dx = 5 #giving a range of values to the speed of the random balls
 minimum_ball_dx = -5
 maximum_ball_dy = 5
 minimum_ball_dy = -5
 BALLS = []
 def attributes():
-		x = random.randint(-screen_width + maximum_ball_radius, screen_width - maximum_ball_radius)
+		x = random.randint(-screen_width + maximum_ball_radius, screen_width - maximum_ball_radius) #making a function that will generate random attributes for the balls
 		y = random.randint(-screen_height + maximum_ball_radius, screen_height - maximum_ball_radius)
 		dx = random.randint(minimum_ball_dx, maximum_ball_dx)
 		while dx == 0:
@@ -32,15 +34,14 @@ def attributes():
 		color = (random.random(), random.random(), random.random())
 		return(x, y, dx, dy, r, color)
 
-for Balls in range(number_of_balls):
+for Balls in range(number_of_balls): #giving the random balls attributes by using the "attributes" function
 	x, y, dx, dy, r, color = attributes()
 	Ball_1 = Ball(x, y, dx, dy, r, color)
 	BALLS.append(Ball_1)
-BALLS.append(your_ball)
-def move_all_balls():
+def move_all_balls(): #making a function that will move all balls inside the screen
 	for ball in BALLS:
 		ball.move(screen_width, screen_height)
-def collide(ball_a, ball_b):
+def collide(ball_a, ball_b): #making a function that will check for collisions
 		x1 = ball_a.xcor()
 		x2 = ball_b.xcor()
 		y1 = ball_a.ycor()
@@ -52,14 +53,16 @@ def collide(ball_a, ball_b):
 			return True
 		else:
 			return False
-def check_all_balls_collisions():
-	all_balls = []
-	all_balls.append(my_ball)
+def check_all_balls_collisions(): #making a function that uses the function "collide" to check for collisions,
+	all_balls = []				  #and if a colision happens, it will make the bigger ball even bigger and the
+	all_balls.append(my_ball)	  #smaller ball become a new, random ball
+	all_balls.append(your_ball)
 	for ball in BALLS:
 		all_balls.append(ball)
-		for ball_a in all_balls:
-			for ball_b in all_balls:
-				r1 = ball_a.r
+
+	for ball_a in all_balls:
+		for ball_b in all_balls:
+			r1 = ball_a.r
 			r2 = ball_b.r
 			if collide(ball_a, ball_b):
 				x, y, dx, dy, r, color = attributes()
@@ -85,27 +88,43 @@ def check_all_balls_collisions():
 					if your_ball == ball_b:
 						your_ball.r = your_ball.r + 1
 						your_ball.shapesize(your_ball.r/10)
-def UP():
+def UP(): #defining the keys that will be used to move the blue ball (your_ball)
 	your_ball.dy = 5
+	if your_ball.ycor() + your_ball.r > screen_height:
+		your_ball.dy = -your_ball.dy
 def DOWN():
 	your_ball.dy = -5
+	if your_ball.ycor() - your_ball.r < -screen_height:
+		your_ball.dy = -your_ball.dy
 def RIGHT():
 	your_ball.dx = 5
+	if your_ball.xcor() + your_ball.r > screen_width:
+		your_ball.dx = -your_ball.dx
 def LEFT():
 	your_ball.dx = -5
+	if your_ball.xcor() - your_ball.r < -screen_width:
+		your_ball.dx = -your_ball.dx
 turtle.Screen().onkey(UP, "Up")
 turtle.Screen().onkey(DOWN, "Down")
 turtle.Screen().onkey(RIGHT, "Right")
 turtle.Screen().onkey(LEFT, "Left")
 turtle.listen()
-def W():
+def W(): #defining the keys that will be used to move the red ball (my_ball)
 	my_ball.dy = 5
+	if my_ball.ycor() + my_ball.r > screen_height:
+		my_ball.dy = -my_ball.dy
 def S():
 	my_ball.dy = -5
+	if my_ball.ycor() - my_ball.r < -screen_height:
+		my_ball.dy = -my_ball.dy
 def D():
 	my_ball.dx = 5
+	if my_ball.xcor() + my_ball.r > screen_width:
+		my_ball.dx = -my_ball.dx
 def A():
 	my_ball.dx = -5
+	if my_ball.xcor() - my_ball.r < -screen_width:
+		my_ball.dx = -my_ball.dx
 turtle.Screen().onkey(W, "w")
 turtle.Screen().onkey(S, "s")
 turtle.Screen().onkey(D, "d")
@@ -121,6 +140,7 @@ while running:
 	check_all_balls_collisions()
 	my_ball.move(screen_width, screen_height)
 	your_ball.move(screen_width, screen_height)
+	#print(your_ball.dx,your_ball.dy,my_ball.dx,my_ball.dy)
 	turtle.Screen().update()
 	time.sleep(0.05)
 turtle.mainloop()
